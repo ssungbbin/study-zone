91d846d823dc3768f2cbd156c51026991cc311c7
ssungbbin <seongbinbag48@gmail.com>
2025-03-22 12:25:03 +0900

    commit

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+            public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            }
+
+            System.out.println("결과: " + result);
+        }
     }
 }
--- untitled1/src/main/java/org/example/Main.java
+++ untitled1/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       Scanner sc = new Scanner(System.in);
+
+
+       System.out.println("첫번째 정수를 입력하세요:");
+       int a = sc.nextInt();
+       System.out.println("두번째 정수를 입력하세요:");
+       int b = sc.nextInt();
+
+       System.out.println("a와 b의 값은:"+ .plus(a,b));
+       System.out.println("a와 b의 값은:"+ .minus(a,b));
+       System.out.println("a와 b의 값은:"+ .multiply(a,b));
+       System.out.println("a와 b의 값은:"+ .divide(a,b));
     }
 }
