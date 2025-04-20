c6b688af30b02b41677afef4c74b98891e9dd128
ssungbbin <seongbinbag48@gmail.com>
2025-04-20 12:12:45 +0900

    commit

--- untitled1/src/main/java/org/example/Main.java
+++ untitled1/src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import java.util.Scanner;
 
-
-
 public class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
---
+++ untitled1/src/main/java/org/example/number.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("주민번호 입력:");
+        String str = sc.nextLine();
+        char[] ch = str.toCharArray();
+
+        for(int i = 8; i < ch.length; i++) {
+
+                ch[i]= '*';
+
+        }
+        System.out.println(ch);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
